5d2db63f237d1df51332cb850b84a445edbe8e74
addico786 <adnandelhi2004@gmail.com>
2025-07-05 10:49:04 +0000

    final commit

--- websites/urls.py
+++ websites/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 
-admin.site.site_header = "Adnan Admin"
+admin.site.site_header = " Admin"
 admin.site.site_title = "portfolio Admin Portal"
-admin.site.index_title = "Welcome t0 the portfolio admin Portal"
+admin.site.index_title = "Welcome to the portfolio admin Portal"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("portfolio.urls"))
